29661db028a0fa9266c7d4f767b98ece4d8f3a84
deji <dejiwilliams9@gmail.com>
2025-06-11 15:57:57 +0100

    blogs page.

--- src/components/template/ourblog/topSection.tsx
+++ src/components/template/ourblog/topSection.tsx
@@ -5,21 +5,24 @@ import { useState } from "react";
 const TopSection = () => {
     const [ blogContent, setBlogContent ] = useState("")
   return (
-    <div className="mb-[64px]">
-      <h1>Latest Crypto & Gift Card News And Blog</h1>
-      <p>
-        Get expert tips and updates on crypto trading, gift card exchange, and
-        digital finance — all in one place.
-      </p>
-      <Input
-        type="text"
-        placeholder="Search by topic, category"
-        value={blogContent}
-        onChange={(e) => setBlogContent(e.target.value)}
-      />
-      <div className="flex">
+    <div className="mb-[64px] px-[60.5px]">
+      <div className="px-[120px] mb-[48px]">
+        <h1 className="font-[400] text-[64px] leading-[76.8px] text-center mb-[24px]">Latest Crypto & Gift Card News And Blog</h1>
+        <p className="font-[400] text-[24px] leading-[33.6px] text-center mb-[64px]">
+          Get expert tips and updates on crypto trading, gift card exchange, and
+          digital finance — all in one place.
+        </p>
+        <Input
+        className="w-[651px] mx-auto"
+          type="text"
+          placeholder="Search by topic, category"
+          value={blogContent}
+          onChange={(e) => setBlogContent(e.target.value)}
+        />
+      </div>
+      <div className="flex gap-8">
         {List.map((item, index) => (
-          <p key={index}>{item}</p>
+          <p className="border py-[10px] px-[14px] font-[600] text-[16px] inline-flex leading-[25.35px] bg-white border-[#D9D9D9] rounded-[9.05px]" key={index}>{item}</p>
         ))}
       </div>
     </div>
--- src/components/ui/blogcard.tsx
+++ src/components/ui/blogcard.tsx
@@ -10,15 +10,17 @@ interface BlogcardProps{
 
 const Blogcard: React.FC<BlogcardProps> = ({image, title, body, name, date, readTime}) => {
   return (
-    <div className="w-[396px] rounded-[7.65px] p-[22.95px] flex flex-col gap-[15.3px]">
+    <div className="w-[370px] rounded-[7.65px] p-[22.95px] flex flex-col gap-[15.3px]">
       <div><img src={image} loading="lazy" alt={title} /></div>
-      <div className="flex items-center">
+      <div className="flex items-center gap-[11.48px] text-subCyan text-[12px] font-[600] leading-[18.74px]">
         <p>{name}</p>
+        <div className="w-[0.96px] h-full bg-subCyan"></div>
         <p>{date}</p>
+        <div className="w-[0.96px] h-full bg-subCyan"></div>
         <p>{readTime}</p>
       </div>
-      <div>{title}</div>
-      <p>{body}</p>
+      <div className="font-[600] text-[20px] leading-[28px]">{title}</div>
+      <p className="font-[400] text-[14px] leading-[19.6px]">{body}</p>
     </div>
   )
 }
