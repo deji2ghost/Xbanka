d059b735a618cab4e7e95829e7c858b3f804ff18
deji <dejiwilliams9@gmail.com>
2025-06-27 15:17:54 +0100

    fix: deployment.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Footer from './components/layout/footer';
 import { lazy } from 'react';
 import CryptoFinance from './components/layout/cryptolayout/layout';
 import ScrollToTop from './components/ui/scrollToTop';
-import Sellsteamcard from './pages/Sellsteamcard.tsx';
+import SellSteamCard from './pages/Sellsteamcard';
 
 const Home = lazy(() => import("./pages/Home"));
 const RateCalculator = lazy(() => import("./pages/RateCalculator"));
@@ -41,7 +41,7 @@ function AppRoutes() {
           <Route path='sellGiftCardsInNaira' element={<SellNairaCards />} />
           <Route path='sellGiftCardsInCedis' element={<SellCediscard />} />
           <Route path='sellGiftCardsInUSD' element={<SellUSDcard />} />
-          <Route path='sellSteamGiftCard' element={<Sellsteamcard />} />
+          <Route path='sellSteamGiftCard' element={<SellSteamCard />} />
           <Route path='sellGooglePlayGiftCard' element={<SellGooglePlaycard />} />
           <Route path='sellAmazonGiftCard' element={<SellAmazoncard />} />
           <Route path='sellRazerGoldGiftCards' element={<SellRazerGoldcard />} />
